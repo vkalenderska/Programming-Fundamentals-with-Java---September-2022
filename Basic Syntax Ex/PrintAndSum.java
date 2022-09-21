44f9a4619124a845e3dce1315f01c459b4c9594d
Виктория Календерска <114094969+vkalenderska@users.noreply.github.com>
2022-09-21 22:43:40 +0300

    Add some exercises for Basic Syntax
    Solutions of some problems for Basic Syntax

---
+++ Basic Syntax Ex/PrintAndSum.java
@@ -0,0 +1,23 @@
+package BasicEx;
+
+import java.util.Scanner;
+
+public class PrintAndSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int startNumber = Integer.parseInt(scanner.nextLine());
+        int endNumber = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+        for (int number = startNumber; number <= endNumber; number++) {
+            System.out.print(number + " ");
+            sum = sum + number;
+        }
+
+        System.out.println();
+        System.out.println("Sum: " + sum);
+
+
+    }
+}
+
